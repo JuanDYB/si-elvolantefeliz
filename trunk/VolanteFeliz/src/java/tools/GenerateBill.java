0a9c0e10eb08bad30effb21606699a05e3c5695d
jdyb04jdyb@gmail.com <jdyb04jdyb@gmail.com@c531da76-5784-2714-2450-f495d2597cef>
2012-04-13 17:05:58 +0000

    Generacion factura

--- trunk/VolanteFeliz/src/java/tools/GenerateBill.java
+++ trunk/VolanteFeliz/src/java/tools/GenerateBill.java
@@ -1,9 +1,12 @@
 package tools;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import model.Alquiler;
+import model.Cliente;
+import model.Factura;
 import model.Incidencia;
 
 /**
@@ -11,19 +14,33 @@ import model.Incidencia;
  * @author Juan Díez-Yanguas Barber
  */
 public class GenerateBill {
+    Cliente cli;
     String [] alquileres;
     HashMap <String, Alquiler> alquileresFacturar;
     String [] incidencias;
     HashMap <String, Incidencia> incidenciasFacturar;
     HttpServletRequest request;
     BigDecimal precioFinal = BigDecimal.ZERO;
+    BigDecimal precioFinalIVA = BigDecimal.ZERO;
+    private final long MILLSECS_PER_DAY = 24 * 60 * 60 * 1000;
 
-    public GenerateBill(String[] alquileres, HashMap<String, Alquiler> alquileresFacturar, String[] incidencias, HashMap<String, Incidencia> incidenciasFacturar, HttpServletRequest request) {
+    public GenerateBill(Cliente cli, String[] alquileres, HashMap<String, Alquiler> alquileresFacturar, String[] incidencias, HashMap<String, Incidencia> incidenciasFacturar, HttpServletRequest request) {
+        this.cli = cli;
         this.alquileres = alquileres;
         this.alquileresFacturar = alquileresFacturar;
         this.incidencias = incidencias;
         this.incidenciasFacturar = incidenciasFacturar;
         this.request = request;
+    }
+    
+    public Factura generateBill (){
+        WebConfig config = (WebConfig) request.getServletContext().getAttribute("appConfig");
+        this.doLoopAlquileres();
+        this.doLoopIncidences();
+        precioFinalIVA = precioFinal.multiply(new BigDecimal(config.getIVA()));
+        Factura fact = new Factura(Tools.generaUUID(), cli, alquileresFacturar, incidenciasFacturar
+                , config.getIVA(), precioFinal, precioFinalIVA, Tools.getDate(), null, null, false);
+        return fact;
     }
     
     private void doLoopIncidences (){
@@ -45,13 +62,24 @@ public class GenerateBill {
                 request.setAttribute("resultados", "Componentes de factura eliminados");
                 Tools.anadirMensaje(request, "Se ha eliminado el alquiler: " + alquileres[i] + " por resultar ya facturado", 'w');
             }else{
-                
+                precioFinal = precioFinal.add(this.calculateRentPrice(alq));
             }
         }
     }
     
     private BigDecimal calculateRentPrice (Alquiler alq){
-        return null;
+        long diasAlquiler = this.difFechas(alq.getFechaFin(), alq.getFechaInicio());
+        long diasExtra = this.difFechas(alq.getFechaEntrega(), alq.getFechaFin());
+        int combustibleGastado = alq.getVehiculo().getCapacidadCombustible() - alq.getCombustibleFin();
+        
+        BigDecimal precioDiasNormal = alq.getTarifa().getPrecioDia().multiply(new BigDecimal(diasAlquiler));
+        BigDecimal precioDiasExtra = alq.getTarifa().getPrecioDiaExtra().multiply(new BigDecimal(diasExtra));
+        BigDecimal precioCombustible = alq.getTarifa().getPrecioCombustible().multiply(new BigDecimal(combustibleGastado));
+        return alq.getTarifa().getPrecioBase().add(precioDiasNormal.add(precioDiasExtra.add(precioCombustible))); 
     }
     
+    private long difFechas (Date actual, Date pasada){
+        return (actual.getTime() - pasada.getTime()) / MILLSECS_PER_DAY;
+        
+    }   
 }
