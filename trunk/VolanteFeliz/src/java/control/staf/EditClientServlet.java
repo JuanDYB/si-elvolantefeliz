964780a19d8a7476a228c631a0e75ff99edea7be
jdyb04jdyb@gmail.com <jdyb04jdyb@gmail.com@c531da76-5784-2714-2450-f495d2597cef>
2012-05-02 18:46:10 +0000

    Solucionados fallos en la edicion de clientes

--- trunk/VolanteFeliz/src/java/control/staf/EditClientServlet.java
+++ trunk/VolanteFeliz/src/java/control/staf/EditClientServlet.java
@@ -32,6 +32,21 @@ public class EditClientServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.sendError(404);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         if (this.validateForm(request)) {
             try {
                 PersistenceInterface persistence = (PersistenceInterface) request.getServletContext().getAttribute("persistence");
@@ -60,41 +75,26 @@ public class EditClientServlet extends HttpServlet {
                     request.setAttribute("resultados", "Resultados de la operaci&oacute;n");
                     if (ok){
                         Tools.anadirMensaje(request, "El cliente ha sido editado correctamente", 'o');
-                        request.getRequestDispatcher("staf/manageclients.jsp").forward(request, response);
+                        request.getRequestDispatcher("/staf/manageclients.jsp").forward(request, response);
                     } else{
                         Tools.anadirMensaje(request, "ha ocurrido un error editando el cliente", 'e');
-                        request.getRequestDispatcher("staf/editclient.jsp?cli=" + codCliente ).forward(request, response);
+                        request.getRequestDispatcher("/staf/editclient.jsp?cli=" + codCliente).forward(request, response);
                     }
                 }
             } catch (IntrusionException ex){
                 request.setAttribute("resultados", "Detectada una intrusión");
                 Tools.anadirMensaje(request, ex.getUserMessage(), 'w');
-                request.getRequestDispatcher("staf/manageclients.jsp").forward(request, response);
+                request.getRequestDispatcher("/staf/manageclients.jsp").forward(request, response);
             } catch (ValidationException ex) {
                 request.setAttribute("resultados", "Validación del formulario fallida");
                 Tools.anadirMensaje(request, ex.getUserMessage(), 'w');
-                request.getRequestDispatcher("staf/manageclients.jsp").forward(request, response);
+                request.getRequestDispatcher("/staf/manageclients.jsp").forward(request, response);
             }
         } else {
             request.setAttribute("resultados", "Formulario no esperado");
             Tools.anadirMensaje(request, "El formulario recibido no se esperaba", 'w');
-            request.getRequestDispatcher("staf/manageclients.jsp").forward(request, response);
+            request.getRequestDispatcher("/staf/manageclients.jsp").forward(request, response);
         }
-    }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.sendError(404);
     }
 
     /**
@@ -110,7 +110,7 @@ public class EditClientServlet extends HttpServlet {
     private boolean validateForm(HttpServletRequest request) {
         if (request.getParameterMap().size() >= 8 && request.getParameter("codCliente") != null && request.getParameter("name") != null
                 && request.getParameter("address") != null && request.getParameter("phone") != null && request.getParameter("company") != null
-                && request.getParameter("emai") != null && request.getParameter("age") != null && request.getParameter("send") != null) {
+                && request.getParameter("email") != null && request.getParameter("age") != null && request.getParameter("send") != null) {
             return true;
         }
         return false;
