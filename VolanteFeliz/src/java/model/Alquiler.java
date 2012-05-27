4bfc363edc929dbef54a3805d5af5bd31a800e9b
jdyb04jdyb@gmail.com <jdyb04jdyb@gmail.com@c531da76-5784-2714-2450-f495d2597cef>
2012-05-27 17:01:19 +0000

    Alquiler serializable

--- VolanteFeliz/src/java/model/Alquiler.java
+++ VolanteFeliz/src/java/model/Alquiler.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -7,7 +8,7 @@ import java.util.Date;
  *
  * @author Juan Díez-Yanguas Barber
  */
-public class Alquiler {
+public class Alquiler implements Serializable{
     private String codAlquiler;
     private Cliente cliente;
     private Vehiculo vehiculo;
